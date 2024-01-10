670045fb1a4c53d018ed9df14fb95c0402623c97
U-MAGNUM\Beisegul.K <Beisegul.K@GO-DIT-295.magnum.local>
2024-01-10 10:08:24 +0600

    first commit

---
+++ app.js
@@ -0,0 +1,27 @@
+var express = require('express');
+var path = require('path');
+var cookieParser = require('cookie-parser');
+
+var infoRouter = require('./routes/info');
+var defaultRouter = require('./routes/default');
+
+var app = express();
+
+app.get('/', function (req, res) {
+  var name = req.query.name ? req.query.name: 'works! ' + 'UUID: ' 
+  res.send(`TestCronJob, ${name}`)
+})
+
+// Define request response for about page
+app.get('/about', function (req, res) {
+  res.send(`<h3>About page is so cool</h3>`)
+})
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+app.use(cookieParser());
+app.use(express.static(path.join(__dirname, 'public')));
+
+app.use('/info', infoRouter);
+app.use('*', defaultRouter);
+module.exports = app;
